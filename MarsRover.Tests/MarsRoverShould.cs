e4bcf3396e78c3c93897d69903589aede6467d9f
Tom <thomas.spencer@codurance.com>
2020-08-20 18:24:42 +0100

    Initial commit

---
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MarsRover.Tests
+{
+    [TestFixture]
+    public class MarsRoverShould
+    {
+        [Test]
+        public void turns_right_north_to_east()
+        {
+            Rover rover = new Rover("N");
+            Assert.AreEqual("E", rover.Facing);
+        }
+    }
+
+    public class Rover
+    {
+        private string _facing;
+
+        public Rover(string facing)
+        {
+            _facing = facing;
+        }
+
+        public string Facing => "E";
+    }
+}
